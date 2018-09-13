9c09c24734f9bbfd06d9143407eb44f699ff5fa4
YANJIAMEI <469953054@qq.com>
2018-09-13 17:18:21 +0800

    change feed_dict

--- mnist.py
+++ mnist.py
@@ -10,12 +10,12 @@ OUTPUT_NODE = 10
 LAYER1_NODE = 500
 BATCH_SIZE = 100
 WEIGHT_INIT = 1. #初始化所有来源样本的权重
-DISTRIBUTE_NODE_NUM = 2
+DISTRIBUTE_NODE_NUM = 2 #参与的节点数目
 
 LEARNING_RATE_BASE = 0.8
 LEARNING_RATE_DECAY = 0.99
 REGULARIZATION_RATE = 0.0001
-TRAINING_STEPS = 3000
+TRAINING_STEPS = 1000
 MOVING_AVERAGE_DECAY = 0.99
 
 
@@ -48,7 +48,7 @@ def train(mnist, mnist_datasets):
     )
     #add weights_instance 表示各节点之间的权重 [0.5 0.5]表示一开始的local和node1之间接受一半的train数据
     weights_node = tf.Variable(
-        tf.constant(50, shape = [2]), trainable = True
+        tf.constant(BATCH_SIZE//DISTRIBUTE_NODE_NUM, shape = [2]), trainable = True
     )
     
     #计算前向传播的结果
@@ -91,6 +91,12 @@ def train(mnist, mnist_datasets):
         # 准备验证数据。一般在神经网络的训练过程中会通过验证数据要大致判断停止的
         # 条件和评判训练的效果。
         validate_feed = {
+            x: mnist.validation.images,
+            y_: mnist.validation.labels
+            # x: mnist_datasets.validation_odd.images,
+            # y_:  mnist_datasets.validation_odd.labels            
+        }
+        validate_feed_odd = {
             # x: mnist.validation.images,
             # y_: mnist.validation.labels
             x: mnist_datasets.validation_odd.images,
@@ -105,16 +111,54 @@ def train(mnist, mnist_datasets):
             # x: mnist_datasets.test_odd.images,
             # y_:  mnist_datasets.test_odd.labels
         }
-
+        test_feed_odd = {
+            # x: mnist.test.images,
+            # y_: mnist.test.labels
+            x: mnist_datasets.test_odd.images,
+            y_:  mnist_datasets.test_odd.labels
+        }
         # 认真体会这个过程，整个模型的执行流程与逻辑都在这一段
         # 迭代的训练神经网络
 
         # M：local数据13579先训练一个batch
-        xs, ys = mnist_datasets.train_odd.next_batch(BATCH_SIZE)
-        sess.run(train_step, feed_dict={x: xs, y_: ys})
-        for i in range(TRAINING_STEPS):
+        # xs, ys = mnist_datasets.train_odd.next_batch(BATCH_SIZE)
+        # sess.run(train_step, feed_dict={x: xs, y_: ys})
+
+        #先训练local数据 至收敛
+        LOCAL_TRAIN_STEP = 100
+        OTHER_TRAIN_STEP = 10
+        for i in range(LOCAL_TRAIN_STEP):
             # 每1000轮输出一次在验证数据集上的测试结果
-            if i % 1000 == 0:
+            if i % 10 == 0:
+                # 计算滑动平均模型在验证数据上的结果。因为MNIST数据集比较小，所以一次
+                # 可以处理所有的验证数据。为了计算方便，本样例程序没有将验证数据划分为更
+                # 小的batch。当神经网络模型比较复杂或者验证数据比较大时，太大的batch
+                # 会导致计算时间过长甚至发生内存溢出的错误。
+                # 注意我们用的是滑动平均之后的模型来跑我们验证集的accuracy
+                validate_acc = sess.run(accuracy, feed_dict=validate_feed)
+                print("After %d training step(s), validation accuracy-all is %g " % (i, validate_acc))
+                validate_acc_odd = sess.run(accuracy, feed_dict = validate_feed_odd)
+                print("After %d training step(s), validation accuracy-odd is %g \n" % (i, validate_acc_odd))
+            # 产生这一轮使用的一个batch的训练数据，并运行训练过程。
+            # xs, ys = mnist.train.next_batch(BATCH_SIZE)
+            # sess.run(train_step, feed_dict={x: xs, y_: ys})
+            # M: 加入24680 传输数据继续训练
+            w = sess.run(weights_node)
+            xs, ys = mnist_datasets.train_odd.next_batch(BATCH_SIZE)
+            # xo, yo = mnist_datasets.train_even.next_batch(w[1])
+            # xc = np.vstack((xs,xo))
+            # yc = np.vstack((ys,yo))
+            # # Shuffle the data
+            # perm = np.arange(w[0]+w[1])
+            # np.random.shuffle(perm)
+            # xc = xc[perm]
+            # yc = yc[perm]
+            sess.run(train_step, feed_dict={x: xs, y_: ys})
+        
+        #计算local和邻接数据的KL-divergence之后 加入邻接数据训练
+        for i in range(OTHER_TRAIN_STEP):
+            # 每1000轮输出一次在验证数据集上的测试结果
+            if i % 10 == 0:
                 # 计算滑动平均模型在验证数据上的结果。因为MNIST数据集比较小，所以一次
                 # 可以处理所有的验证数据。为了计算方便，本样例程序没有将验证数据划分为更
                 # 小的batch。当神经网络模型比较复杂或者验证数据比较大时，太大的batch
@@ -128,33 +172,24 @@ def train(mnist, mnist_datasets):
             # sess.run(train_step, feed_dict={x: xs, y_: ys})
             # M: 加入24680 传输数据继续训练
             w = sess.run(weights_node)
-            xs, ys = mnist_datasets.train_odd.next_batch(w[0])
-            xo, yo = mnist_datasets.train_even.next_batch(w[1])
-            xc = np.vstack((xs,xo))
-            yc = np.vstack((ys,yo))
+            xo, yo = mnist_datasets.train_odd.next_batch(w[0])
+            xe, ye = mnist_datasets.train_even.next_batch(w[1])
+            xc = np.vstack((xe,xo))
+            yc = np.vstack((ye,yo))
+            # Shuffle the data
+            perm = np.arange(w[0]+w[1])
+            np.random.shuffle(perm)
+            xc = xc[perm]
+            yc = yc[perm]
             sess.run(train_step, feed_dict={x: xc, y_: yc})
-
         # 在训练结束之后，在测试数据上检测神经网络模型的最终正确率。
         # 同样，我们最终的模型用的是滑动平均之后的模型，从这个accuracy函数
         # 的调用就可以看出来了，因为accuracy只与average_y有关
         test_acc = sess.run(accuracy, feed_dict=test_feed)
-        print("After %d training step(s), test accuracy is %g" % (TRAINING_STEPS, test_acc))
+        print("After %d training step(s), test accuracy-all is %g" % (LOCAL_TRAIN_STEP + OTHER_TRAIN_STEP, test_acc))
+        test_acc_odd = sess.run(accuracy,feed_dict=test_feed_odd)
+        print("After %d training step(s), test accuracy-odd is %g" % (LOCAL_TRAIN_STEP + OTHER_TRAIN_STEP, test_acc_odd))
 
-def extract_n_data_sets(datasets, label = [1,2,3]):
-    test_data_set_image = datasets.images
-    test_data_set_label = datasets.labels
-    extract_images = np.array([])
-    extract_labels = np.array([])
-    cnt = 0
-    for i in range(datasets.num_examples):
-        if (np.argmax (test_data_set_label[i]) in label):
-            cnt += 1
-            extract_images = np.append(extract_images , test_data_set_image[i])
-            extract_labels = np.append(extract_labels , test_data_set_label[i])
-    extract_images = extract_images.astype(np.float32)
-        # return mnist.DataSet(extract_images, extract_labels, dtype = dtypes.float32, reshape = True)
-    return mnist.DataSet(extract_images.reshape(cnt,784), extract_labels.reshape(cnt,10)
-                        , dtype = dtypes.uint8, reshape = False)
 # 主程序入口
 Datasets = collections.namedtuple('Datasets', ['train_odd', 'train_even'
                                     , 'test_odd', 'validation_odd'])
@@ -162,15 +197,16 @@ def main(argv=None):
     # 声明处理MNIST数据集的类，这个类在初始化时会自动下载数据。
     mnist = input_data.read_data_sets("./data", one_hot=True)
     # train(mnist)
-    mnist_13579_train = extract_n_data_sets(mnist.test,label=[1,3,5,7,9])
-    mnist_24680_train = extract_n_data_sets(mnist.test,label=[2,4,6,8,0])
-    mnist_13579_validation = extract_n_data_sets(mnist.validation, label=[1,3,5,7,9])
-    mnist_13579_test = extract_n_data_sets(mnist.validation, label=[1,3,5,7,9])
-    mnist_datasets = Datasets(train_odd = mnist_13579_train, train_even = mnist_24680_train
-                                    , test_odd = mnist_13579_test, validation_odd = mnist_13579_validation)
+    # mnist_13579_train = extract_n_data_sets(mnist.test,label=[1,3,5,7,9])
+    # mnist_24680_train = extract_n_data_sets(mnist.test,label=[2,4,6,8,0])
+    # mnist_13579_validation = extract_n_data_sets(mnist.validation, label=[1,3,5,7,9])
+    # mnist_13579_test = extract_n_data_sets(mnist.validation, label=[1,3,5,7,9])
+    mnist_datasets_load = np.load('./data/mnist_datasets.npy')
+    mnist_datasets = Datasets(train_odd = mnist_datasets_load[0], train_even = mnist_datasets_load[1]
+                                    , test_odd = mnist_datasets_load[3], validation_odd = mnist_datasets_load[2])
+    
     train(mnist=mnist, mnist_datasets=mnist_datasets)
-    # x1,y1 = extract_n_data_sets(mnist,label=1)
-    print ('ok')
+    
 # TensorFlow提供的一个主程序入口，tf.app.run会调用上面定义的main函数
 if __name__ == "__main__":
     tf.app.run()
